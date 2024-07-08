a7f953013fcfec1a67877bc314d040e8b35165c6
T3CHTAYL0R <techtaylor92@outlook.com>
2024-07-08 12:23:35 -0400

    A: Enabled User Input to start game

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+
+name = input("Type your name: ")
+print("Welcome,", name)
+
+while True:
+    response = input("Are you ready for your adventure? (yes/no): ").strip().lower()
+
+    if response == "yes":
+        print("Let's Begin")
+        break
+    elif response == "no":
+        print("Maybe next time!")
+        sys.exit()
+
+    else:
+        print("Please enter 'yes' or 'no'.")
