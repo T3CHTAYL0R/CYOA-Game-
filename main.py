fe2aaf673d1ab311598c03f68be1047ef7a09af3
T3CHTAYL0R <techtaylor92@outlook.com>
2024-07-11 15:48:40 -0400

    B: Created Function for setting a class

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ print("Welcome,", name)
 
 while True:
     response = input("Are you ready for your adventure? (yes/no): ").strip().lower()
-
     if response == "yes":
         print("Let's Begin")
         break
